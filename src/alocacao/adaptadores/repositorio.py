a1d401b253f9111088382dc4591df7ce485580cf
ralphribeiro <ralph_ribeiro@msn.com>
2021-05-03 11:29:42 -0300

    ajuste na interface e repositorio falso

--- src/alocacao/adaptadores/repositorio.py
+++ src/alocacao/adaptadores/repositorio.py
@@ -4,17 +4,21 @@ from abc import abstractmethod
 from src.alocacao.dominio import modelo
 
 
-class RepositorioAbstrato():
+class RepositorioAbstrato():  # porta
     @abstractmethod
-    def insere(self, lote: modelo.Lote):
+    def add(self, lote: modelo.Lote):
         ...
 
     @abstractmethod
-    def obtem(self, referencia) -> modelo.Lote:
+    def get(self, ref) -> modelo.Lote:
+        ...
+
+    @abstractmethod
+    def list_all(self) -> list[modelo.Lote]:
         ...
 
 
-class SQLAlchemyRepositorio(RepositorioAbstrato):
+class SQLAlchemyRepositorio(RepositorioAbstrato):  # adaptador
     def __init__(self, session) -> None:
         self.session = session
 
@@ -24,5 +28,19 @@ class SQLAlchemyRepositorio(RepositorioAbstrato):
     def get(self, ref) -> modelo.Lote:
         return self.session.query(modelo.Lote).filter_by(ref=ref).one()
 
-    def list_all(self):
+    def list_all(self) -> list[modelo.Lote]:
         return self.session.query(modelo.Lote).all()
+
+
+class FalsoRepositorio(RepositorioAbstrato):  # adaptador
+    def __init__(self, lotes):
+        self._lotes = set(lotes)
+
+    def add(self, lote: modelo.Lote):
+        self._lotes.add(lote)
+
+    def get(self, ref) -> modelo.Lote:
+        return next(l for l in self._lotes if l == ref)
+
+    def list_all(self) -> list[modelo.Lote]:
+        return list(self._lotes)
